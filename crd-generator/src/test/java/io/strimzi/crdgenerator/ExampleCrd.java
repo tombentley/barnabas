c787a5e0b8c3b49e5b6ef3ca999d235001420c01
Tom Bentley <tbentley@redhat.com>
2020-09-15 16:48:10 +0100

    wip
    Signed-off-by: Tom Bentley <tbentley@redhat.com>

--- crd-generator/src/test/java/io/strimzi/crdgenerator/ExampleCrd.java
+++ crd-generator/src/test/java/io/strimzi/crdgenerator/ExampleCrd.java
@@ -22,7 +22,6 @@ import java.util.List;
 import java.util.Map;
 
 @Crd(
-    apiVersion = "apiextensions.k8s.io/v1beta1",
     spec = @Crd.Spec(
         group = "crdgenerator.strimzi.io",
         names = @Crd.Spec.Names(
@@ -30,7 +29,6 @@ import java.util.Map;
             plural = "examples",
             categories = {"strimzi"}),
         scope = "Namespaced",
-        version = "v1alpha1",
     versions = {
         @Crd.Spec.Version(name = "v1alpha1", served = true, storage = true),
         @Crd.Spec.Version(name = "v1beta1", served = true, storage = false)
